74bb17b7517b44732a3c3827d6c34a520023ffd5
stheven <victor.stheven@consultornet.com.br>
2019-01-15 01:33:45 -0200

    add ionic

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,8 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'app-todo',
+  globalScript: 'src/global/app.ts',
+  globalStyle: 'src/global/app.css',
   outputTargets:[
     { type: 'dist' },
     { type: 'docs' },
